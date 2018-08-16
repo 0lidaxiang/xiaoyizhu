35e9f4551db345d0f33cf5003e90e4b44c767683
0lidaxiang <0lidaxiang@gmail.com>
2018-08-16 13:40:28 +0800

    update for 8.12 and 8.5

--- gg_bd_ad_720x90.js
+++ gg_bd_ad_720x90.js
@@ -1,8 +1,8 @@
-﻿document.writeln("<div style=\"width:970px;margin:10px auto;\">")
+﻿document.writeln("<div style=\"width:1170px;margin:10px auto;\">")
 document.writeln('<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>');
 document.writeln('<!-- html5tricks-demo-big -->');
 document.writeln('<ins class="adsbygoogle"');
-document.writeln('     style="display:inline-block;width:970px;height:250px"');
+document.writeln('     style="display:inline-block;width:1170px;height:250px"');
 document.writeln('     data-ad-client="ca-pub-4188263447419139"');
 document.writeln('     data-ad-slot="5506515209"></ins>');
 document.writeln('<script>');
